85f70a5d865e01dd371ba88d0d09ca0376733918
LeonLawrence <leonlawrence@outlook.com>
2022-03-12 20:27:48 +0000

    Add User Function coded

--- src/main/java/com/mycompany/user/UserController.java
+++ src/main/java/com/mycompany/user/UserController.java
@@ -9,7 +9,8 @@ import java.util.List;
 
 @Controller
 public class UserController {
-    @Autowired private UserService service;
+    @Autowired
+    private UserService service;
 
     @GetMapping("/users")
     public String showUserList(Model model) {
@@ -17,5 +18,11 @@ public class UserController {
         model.addAttribute("listUsers", listUsers);
 
         return "users";
+    }
+
+    @GetMapping("/users/new")
+    public String showNewForm(Model model) {
+        model.addAttribute("user", new User());
+        return "user_form";
     }
 }
